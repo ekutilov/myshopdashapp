b42db84d970882804d97968169b10c49ce33af79
Eugene Kutilov <ekutilov@gmail.com>
2025-02-11 21:28:38 +1100

    parametrised SQL to prevent ijections

--- backend/cloud_functions/parse_v3/msd_data/enrichmentdata.py
+++ backend/cloud_functions/parse_v3/msd_data/enrichmentdata.py
@@ -1,12 +1,17 @@
 """ 
 TODO 
 """
-from google.cloud.bigquery import Client as BQClient 
+
+from google.cloud.bigquery import Client as BQClient,\
+    QueryJobConfig, ScalarQueryParameter, ArrayQueryParameter
 
 from .env import ENV, PROJECT_ID
 
 
 class EnrichmentData():
+    """
+    Class to enrich the data with additional information from the database.
+    """
     def __init__(self, data, match_type="sku"):
         self.data = data
         self.metabrand = data.metabrand
@@ -16,13 +21,20 @@ class EnrichmentData():
         self.transactions_enrichment = self.get_transactions_enrichment_data()
 
     def get_items_enrichment_data(self, match_type="sku"):
-
+        """
+        Get the items enrichment data from the database.
+        """
         if match_type=="name":
-            in_clause_items = lambda data: '( "' +\
-                '","'.join(data.items.Product.str.extract(r"^\#?(?P<Product>.*)$").squeeze().unique().tolist()) +\
-                '")'
+            job_config = QueryJobConfig(
+                query_parameters=[
+                    ScalarQueryParameter("metabrand", "STRING", self.data.metabrand),
+                    ArrayQueryParameter("receipt_name", "STRING", 
+                        self.data.items.Product.str.extract(r"^\#?(?P<Product>.*)$").squeeze().unique().tolist()
+                    ),
+                ]
+            )
 
-            q_items = lambda data, metabrand: f"""
+            q_items = f"""
                 SELECT
                      m.receipt_name
                     ,m.sku
@@ -54,60 +66,72 @@ class EnrichmentData():
                 FROM `{PROJECT_ID}.{ENV.lower()}.rcpt2sku_map` m
                 LEFT JOIN `{PROJECT_ID}.{ENV.lower()}.products` p
                 ON m.sku=p.sku AND p.metabrand=m.metabrand
-                WHERE m.metabrand="{metabrand}"
-                AND m.receipt_name IN {in_clause_items(data)}
+                WHERE m.metabrand= @metabrand
+                AND m.receipt_name IN UNNEST(@receipt_name)
+            """
+
+            return self.client.query(q_items, job_config=job_config).to_dataframe()
+
+
+        job_config = QueryJobConfig(
+            query_parameters=[
+                ScalarQueryParameter("metabrand", "STRING", self.data.metabrand),
+                ArrayQueryParameter("sku", "STRING", self.data.items['Sku'].unique().tolist()),
+            ]
+        )
+
+        q_items = f"""
+            SELECT
+                 p.sku
+                ,p.display_name
+                ,p.product_name
+                ,p.department
+                ,p.department_id
+                ,p.category
+                ,p.subcategory
+                ,p.aisle
+                ,p.orig_deparment
+                ,p.orig_category
+                ,p.brand
+                ,p.country_of_origin
+                ,p.package_size
+                ,p.package_size_unit
+                ,p.unit_price
+                ,p.cup_price
+                ,p.cup_measure
+                ,p.cup_price_desc
+                ,p.nutrition_plate
+                ,p.big_image_url
+                ,p.small_image_url
+                ,p.product_url
+                ,p.url_friendly_name
+                ,p.msd_product_key
+                ,p.barcode
+                ,p.receipt_name
+            FROM `{PROJECT_ID}.{ENV.lower()}.products` p
+            WHERE p.metabrand=@metabrand
+            AND p.sku IN UNNEST(@sku)
         """
 
-        elif match_type=="sku":
-            in_clause_items = lambda content: '( "' +\
-                '","'.join(content.items['Sku'].unique().tolist()) +\
-                '")'
-
-            q_items = lambda data, metabrand: f"""
-                SELECT
-                     p.sku
-                    ,p.display_name
-                    ,p.product_name
-                    ,p.department
-                    ,p.department_id
-                    ,p.category
-                    ,p.subcategory
-                    ,p.aisle
-                    ,p.orig_deparment
-                    ,p.orig_category
-                    ,p.brand
-                    ,p.country_of_origin
-                    ,p.package_size
-                    ,p.package_size_unit
-                    ,p.unit_price
-                    ,p.cup_price
-                    ,p.cup_measure
-                    ,p.cup_price_desc
-                    ,p.nutrition_plate
-                    ,p.big_image_url
-                    ,p.small_image_url
-                    ,p.product_url
-                    ,p.url_friendly_name
-                    ,p.msd_product_key
-                    ,p.barcode
-                    ,p.receipt_name
-                FROM `{PROJECT_ID}.{ENV.lower()}.products` p
-                WHERE p.metabrand="{metabrand}"
-                AND p.sku IN {in_clause_items(data)}
-        """
-
-        items_enrichment = self.client.query(q_items(self.data, self.data.metabrand)).to_dataframe()
+        items_enrichment = self.client.query(q_items, job_config=job_config).to_dataframe()
 
         return items_enrichment
 
     def get_transactions_enrichment_data(self, match_type=None):
-        in_clause_transactions = lambda data: '( "' +\
-            '","'.join(data.transactions['Store Number'].unique().tolist()) +\
-            '")'
+        """
+        Get the transactions enrichment data from the database.
+        """
+        job_config = QueryJobConfig(
+            query_parameters=[
+                ScalarQueryParameter("metabrand", "STRING", self.data.metabrand),
+                ArrayQueryParameter("number", "STRING",
+                    self.data.transactions['Store Number'].unique().tolist()
+                ),
+            ])
 
-        q_stores = lambda data, metabrand: f"""
+        q_stores = f"""
             SELECT
-                brand
+                 brand
                 ,brand_cd
                 ,brand_id
                 ,division
@@ -124,10 +148,10 @@ class EnrichmentData():
                 ,facilities
                 ,trading_hours
             FROM `{PROJECT_ID}.{ENV.lower()}.stores`
-            WHERE metabrand="{metabrand}"
-            AND number IN {in_clause_transactions(data)}
+            WHERE metabrand=@metabrand
+            AND number IN UNNEST(@number)
         """
 
-        transactions_enrichment = self.client.query(q_stores(self.data,self.data.metabrand)).to_dataframe()
+        transactions_enrichment = self.client.query(q_stores, job_config=job_config).to_dataframe()
 
         return transactions_enrichment
